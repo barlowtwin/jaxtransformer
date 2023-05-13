e680c18a18fb28ad652056063869babf5287418f
Sarang <sarang7m@gmail.com>
2023-05-13 23:56:45 +0530

    Add files via upload

--- unit_test.py
+++ unit_test.py
@@ -2,7 +2,8 @@
 import numpy as np
 import jax.numpy as jnp
 from attention import scaled_dot_attention
-
+from attention import MultiheadAttention
+from jax import random
 
 # testing scaled dot attention for multi head sqeuences
 
@@ -16,3 +17,17 @@ v = jnp.asarray(v)
 
 ans = scaled_dot_attention(q, k, v)
 print(ans.shape)
+if ans.shape == (10,10,5,5):
+    print("scaled dot attention test passed")
+
+
+
+# testing multihead attention module
+
+main_rng = random.PRNGKey(seed = 42)
+main_rng, x_rng = random.split(main_rng)
+x = np.random.rand(4, 16, 128)
+x = jnp.asarray(x)
+params = mh_attn.init(init_rng, x)['params']
+mh_attn = MultiheadAttention(embed_dim = 128, num_heads = 4)
+out = mh_attn.apply(x)
